cb4ae569ce2fd8765421c2bfd9e686f795c159f1
jangrubin <wkdfnqls2465@gmail.com>
2023-04-12 17:44:29 +0900

    프로젝트 시작을 위한 디렉토리 제작

---
+++ JS/index.js
@@ -0,0 +1,5 @@
+// ! JS/index_module.js 의 모듈 js파일을 받아 올 index.js
+import divide from "/JS/index_module/divide.js";
+import plus from "/JS/index_module/plus.js";
+import multiply from "/JS/index_module/multiply.js";
+import minus from "/JS/index_module/minus.js";
